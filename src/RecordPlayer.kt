c4c146d4ac8e624482d406cb46472c7854a9faf9
MiloszSolent <0jasim37@solent.ac.uk>
2025-10-30 15:54:26 +0000

    updated task 3

--- src/RecordPlayer.kt
+++ src/RecordPlayer.kt
@@ -1,5 +1,10 @@
 class RecordPlayer {
     fun play(music : Music) : String {
-        return music.getPlayingTime().toString() + " " + music.getAllSongs()
+        val allSongsDetails = mutableListOf<String>()
+        for (song in music.getAllSongs()){
+            allSongsDetails.add(song.artist)
+            allSongsDetails.add(song.title)
+        }
+        return music.getPlayingTime().toString() + " " + allSongsDetails
     }
 }
